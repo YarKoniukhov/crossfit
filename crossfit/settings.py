e27b3e7e771064c5693f6bb63ffa40d7394e4a55
yarkoniukvoh <yar.koniukhov@gmail.com>
2022-07-31 18:00:55 +0300

    change debug

--- crossfit/settings.py
+++ crossfit/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# Make Debug
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -41,7 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'backend',
+    'db',
 ]
 
 MIDDLEWARE = [
@@ -82,6 +85,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+      #  'PORT': '5432',
+       # 'USER': 'postgres',
+       # 'PASSWORD': '',
+       # 'HOST': 'localhost'
     }
 }
 
